42ef7ba4c84fd9ba38a946b51a5d73e34a61731a
naoshi20 <naoshi.st.paul@gmail.com>
2023-05-30 21:55:13 +0900

    Prettierを導入しESLintとの競合も解決

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
+  extends: [
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ], // eslint-config-prettierプラグインでconflictを防ぐ
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
+    'no-var-requires': 'null'
   }
 }
