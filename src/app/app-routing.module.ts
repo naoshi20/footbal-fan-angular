9acb77cad1a9d335c3526f59d5a07b40f4ec523b
naoshi20 <naoshi.st.paul@gmail.com>
2023-06-04 11:42:09 +0900

    linterによる修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PlayerDetailComponent } from './player-detail/player-detail.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { PlayerDetailComponent } from './player-detail/player-detail.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/player', pathMatch: 'full' },
-  { path: 'player', component: PlayerDetailComponent },
-];
+  { path: 'player', component: PlayerDetailComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
