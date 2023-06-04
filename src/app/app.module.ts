56db561890eb6dfc09c6b31c5834c3220d1e7bc1
naoshi20 <naoshi.st.paul@gmail.com>
2023-06-04 11:44:35 +0900

    ボタンコンポーネントを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PlayerDetailComponent } from './player-detail/player-detail.component';
 import { PlayerDetailService } from './service/player-detail.service';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, PlayerDetailComponent],
+  declarations: [AppComponent, PlayerDetailComponent, ButtonComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [PlayerDetailService],
   bootstrap: [AppComponent],
