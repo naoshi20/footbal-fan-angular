cd3acedd257d3124445007ddd07e452750c8d324
naoshi20 <naoshi.st.paul@gmail.com>
2023-06-04 13:15:13 +0900

    画面遷移を実装

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { PlayerDetailComponent } from './player-detail/player-detail.component'
+import { ButtonComponent } from './button/button.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/player', pathMatch: 'full' },
-  { path: 'player', component: PlayerDetailComponent }
+  { path: 'player', component: PlayerDetailComponent },
+  { path: 'button', component: ButtonComponent }
 ]
 
 @NgModule({
